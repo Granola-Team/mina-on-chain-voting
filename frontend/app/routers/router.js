303de833a0cd26a87707a0f73ebc37f51bf46c15
Shailesh B Nair <shai567@live.com>
2022-05-26 17:18:23 +0530

    Updated router

--- frontend/app/routers/router.js
+++ frontend/app/routers/router.js
@@ -3,10 +3,19 @@ let router = express.Router();
  
 const customers = require('../controllers/controller.js');
 
+const statistics = require('../controllers/statistics_controller.js');
+
 router.post('/api/customer', customers.createCustomer);
 router.get('/api/customer/:id', customers.getCustomer);
 router.get('/api/customers', customers.customers);
 router.put('/api/customer', customers.updateCustomer);
 router.delete('/api/customer/:id', customers.deleteCustomer);
+
+router.post('/api/statistics', statistics.createUserStatistics);
+router.get('/api/statistics/:id', statistics.getUserStatistics);
+router.get('/api/statistics', statistics.UserStatistics);
+router.put('/api/statistic', statistics.updateUserStatistics);
+router.delete('/api/statistic/:id', statistics.deleteUserStatistics);
+
 
 module.exports = router;

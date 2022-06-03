a1d1402af13cbf15c89891a626c81e0140b0b2bc
root <root@ip-172-31-17-20.ca-central-1.compute.internal>
2022-06-03 09:38:39 +0000

    Modified scheduler to include graphql with issues

--- graphql/src/main.rs
+++ graphql/src/main.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use tokio::time::{sleep, Duration};
 // use JobSchedule;
 use mina_graphql_rs::scheduler::*;
+use mina_graphql_rs::*;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("start {}", Local::now().time());
@@ -13,6 +14,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
             "1/10 * * * * *".to_string(),
             Box::new(|_i| {
                 Box::pin(async move {
+                let percentages = get_staking_percentages("B62qrHzjcZbYSsrcXVgGko7go1DzSEBfdQGPon5X4LEGExtNJZA4ECj", 0).await.unwrap();
+                println!("{:?}", percentages); 
                     println!("1/10 {}", Local::now().time());
                     sleep(Duration::from_secs(10)).await;
                 })

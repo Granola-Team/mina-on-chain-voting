5ad53bab11d709a44f4d8993a4a5a7707b0ced19
root <root@ip-172-31-17-20.ca-central-1.compute.internal>
2022-06-07 13:22:32 +0000

    Changes done in graphql lib

--- graphql/src/main.rs
+++ graphql/src/main.rs
@@ -7,6 +7,14 @@ use mina_graphql_rs::scheduler::*;
 use mina_graphql_rs::*;
 use std::collections::HashMap;
 use anyhow::{anyhow, bail, Result};
+use postgres::{Client, NoTls};
+
+struct Author {
+    _id: i32,
+    name: String,
+    country: String
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("start {}", Local::now().time());
@@ -19,9 +27,63 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 Box::pin(async move {
                // let percentages = get_staking_percentages("B62qrHzjcZbYSsrcXVgGko7go1DzSEBfdQGPon5X4LEGExtNJZA4ECj", 0).await.unwrap();
                // println!("{:?}", percentages); 
-             
+               
                let payments_performed = get_total_payments_performed_single("B62qpG8jpefptAeT9en6WoPzaW312rMJLwoL8pRmqcS8FCKiixRjofg", "B62qrHzjcZbYSsrcXVgGko7go1DzSEBfdQGPon5X4LEGExtNJZA4ECj").await;
-               // payments_performed_for_address.get_mut("B62qpG8jpefptAeT9en6WoPzaW312rMJLwoL8pRmqcS8FCKiixRjofg").ok_or(anyhow!("should have gotten address in map")) += payments_performed; 
+           
+                let mut client = Client::connect("postgresql://postgres:postgres@localhost:5432/postgres", NoTls).unwrap();
+
+    // Create the table in the postgres Database.
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS author (
+            id              SERIAL PRIMARY KEY,
+            name            VARCHAR NOT NULL,
+            country         VARCHAR NOT NULL
+            )
+    ");
+
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS book  (
+            id              SERIAL PRIMARY KEY,
+            title           VARCHAR NOT NULL,
+            author_id       INTEGER NOT NULL REFERENCES author
+            )
+    ");
+
+    // Insert the values to the tables in the Postgres database.
+    let mut authors = HashMap::new();
+    authors.insert(String::from("Pawan Bisht"), "United States");
+    authors.insert(String::from("Pankaj Chaudhary "), "India");
+    authors.insert(String::from("Lokesh Aggarwal"), "singapore");
+    
+    for (key, value) in &authors {
+        let author = Author {
+            _id: 0,
+            name: key.to_string(),
+            country: value.to_string()
+        };
+
+        client.execute(
+            "INSERT INTO author (name, country) VALUES ($1, $2)",
+            &[&author.name, &author.country],
+        );
+    }
+
+    // Retrieve the data from the table.
+    for row in client.query("SELECT id, name, country FROM author", &[]) {
+        let author = Author {
+            _id: row.get(0).unwrap(),
+            name: row.get(1).unwrap(),
+            country: row.get(2).unwrap(),
+        };
+        println!("Author {} is from {}", author.name, author.country);
+    }
+
+
+    // Delete the table from the Postgres database.
+    client.batch_execute("
+        DROP TABLE author, book;
+    "); 
+            // payments_performed_for_address.get_mut("B62qpG8jpefptAeT9en6WoPzaW312rMJLwoL8pRmqcS8FCKiixRjofg").ok_or(anyhow!("should have gotten address in map")) += payments_performed; 
            //    let diffs = verify_enough_payments(
              //        8,
                //     "B62qrHzjcZbYSsrcXVgGko7go1DzSEBfdQGPon5X4LEGExtNJZA4ECj",

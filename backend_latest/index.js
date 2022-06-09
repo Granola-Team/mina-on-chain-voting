46727ea0d631a92b29754ce44e3a0de933511ecb
root <root@ip-172-31-17-20.ca-central-1.compute.internal>
2022-06-09 17:50:46 +0000

    Added backend lib

---
+++ backend_latest/index.js
@@ -0,0 +1,85 @@
+const { Client } = require('pg');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyparser.json());
+app.use(cors());
+
+const client = new Client({
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "postgres"
+})
+
+client.connect((err) => {
+    if(!err)
+    console.log("Db connection successful");
+    else
+    console.log("Db connections fails \n Error: " + Json.stringify(err, undefined, 2));
+});
+
+app.listen(8000, () => console.log('Exress server up'));
+
+//get all users
+app.get('/authors', (req,res) => {
+    client.query('SELECT * FROM author', (err, response) => {
+        if(!err)
+        res.send(response.rows);
+        else
+        console.log(err);   
+    })
+});
+
+//get a user 
+app.get('/users/:id', (req,res) => {
+    const id = req.params.id;
+    client.query(`SELECT * FROM users WHERE userID = ${id}`, (err, response) => {
+        if(!err)
+        res.send(response.rows);
+        else
+        console.log(err);   
+    })
+});
+
+//add a new user
+app.post('/users', (req,res) => {
+    const { userID, name, email} = req.body;
+    console.log(req.body);
+    client.query(`INSERT INTO users (userID, name, email) VALUES (${userID}, '${name}', '${email}') `, (err,response) => {
+        if(!err)
+        res.send("User added Succesfully");
+        else
+        console.log(err);   
+
+    });
+});
+
+//delete a user
+app.delete('/users/:id', (req,res) => {
+    const id = req.params.id;
+    client.query(`DELETE FROM users WHERE userID = ${id}`, (err, response) => {
+        if(!err)
+        res.send("Deleted Succesfully");
+        else
+        console.log(err);   
+    })
+});
+
+//update a user
+app.put('/users/:id', (req,res) => {
+    const id = req.params.id;
+    
+    const { userID, name, email} = req.body;
+    console.log(req.body);
+    client.query(`UPDATE users SET name = '${name}', email='${email}' `, (err, response) => {
+        if(!err)
+        res.send("User Updated Succesfully");
+        else
+        console.log(err);   
+    })
+});
+

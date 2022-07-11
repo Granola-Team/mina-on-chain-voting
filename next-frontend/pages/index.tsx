5d5ccc18a54d7d11be61012aeb02069630f511da
jstavh <106464930+jstavh@users.noreply.github.com>
2022-07-11 16:07:49 -0400

    Update index.tsx

--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Datatable from '../components/Datatable'; 
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>

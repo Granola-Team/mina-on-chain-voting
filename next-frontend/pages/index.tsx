cf10ecc493da47a3557f13b80677ed1d891e773e
jstavh <106464930+jstavh@users.noreply.github.com>
2022-07-11 17:05:42 -0400

    Update index.tsx

--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Datatable from '../components/Datatable'; 
+import Datatable from '../components/datatable'; 
 
 const Home: NextPage = () => {
   return (

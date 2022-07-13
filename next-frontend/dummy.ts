c799211a4f47ac3ac9c38714a4fad140286ccb89
jstavh <jstavh@gmail.com>
2022-07-13 18:49:50 -0400

    just need no-cors for be/fe connection

--- next-frontend/dummy.ts
+++ next-frontend/dummy.ts
@@ -1,7 +1,13 @@
 const dummyData : string[][] = [
-    ["123a5fsdf", "magenta"],
-    ["adf87sda8","no magenta"],
-    ["asdf87adf89","magenta"]
+    ["B62qkUCzCdJzVjQYgBcuzSDbiQkSidXc1axn6TGvRFzhCR1bSeYfkUn", "magenta"],
+    ["B62qiksY5ruYvgRaKkj47AhAA77ypiinXwgxdcbknnaEk1vTDygqUKU", "no magenta"],
+    ["B62qnRkPAoX5ruTEcRYPXZgynj7z6eVRcpozaLm4ixXhYpEbZc4jW5S", "magenta"],
+    ["B62qnKyRWWN95krAkj4mL3NEXEPQ5h6JktDovA9g5bvDRPyxQ9ngAoE", "no-magenta"],
+    ["B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5GRP3xQ", "Magenta"],
+    ["B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5Gkj4mL", "No magenta"],
+    ["B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G8dR5t", "no_magenta"],
+    ["B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5Gq2Wpe", "maegenta"],
+    ["B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", "noMagenta"]
 ]
 
 export default dummyData
--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -1,9 +1,10 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import React, { useState, useEffect, memo } from 'react';
+import NextCors from 'nextjs-cors'; 
 import {Datatable} from '../components/datatable'; 
 import dummyData from '../dummy'
 
@@ -11,37 +12,39 @@ const Home: NextPage = () => {
 
   const [data, setData] = useState<string[][]>([])  
   const [q, setQ] = useState("") 
+  
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.set('Content-Type', 'application/json');
 
   useEffect(() => {
-    fetch("0.0.0.0:8081") // enter URL from backend 
+    fetch("http://35.203.38.140:8080/votes",   
+        {
+      method: 'CONNECT', 
+      headers: requestHeaders,
+      mode: 'no-cors',
+        }) 
     .then(response => response.json())
-    .then(json => setData(json))
-    .catch(error => setData(dummyData));
-  }, []) // [] is an array of dependencies and when it changes, 
-  // the function fires
-  // but since it is empty it is only done at initilization
+    .then(json => setData(json));
+  }, []) 
 
-  type APIdata = {account : string, memo : string}[]
+  type APIdata = {Account : string, Memo : string}[]
   function transform(rows : string[][]) : APIdata {
-      const data : APIdata = rows.map(row => {return({account: row[0], memo: row[1]})})
+      const data : APIdata = rows.map(row => {return({Account: row[0], Memo: row[1]})})
       return data;
   }
 
   function search(rows : any) {
-    return rows.filter((row : any) => row.memo.toLowerCase().indexOf(q) > -1)
-  } //this will search for "magenta", "no magenta", or whatever the user wants
-    //since they are in count, they should all add up
+    return rows.filter((row : any) => row.Memo.toLowerCase().indexOf(q) > -1)
+  } 
 
   return (
     <div>
       <main className={styles.main}> 
-        <h1 className={styles.title}>
-          HF Voting Table Results
-        </h1>
+      <h2> "This is how you can vote today: " </h2>
       
-      <div>
-        <input type="text" value={q} onChange={(e) => setQ(e.target.value)}/>
-      </div>
+      <h1 className={styles.title}>
+          Voting Summary
+        </h1>
       <div>
           <Datatable data={transform(data)} filter={search} /> 
       </div>

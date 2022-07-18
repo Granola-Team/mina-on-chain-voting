98dfbaf79dc19c0038771662c5880e111acab27e
jstavh <jstavh@gmail.com>
2022-07-18 17:39:14 -0400

    FE working with new dummy data and set-up

--- app/dummy.tsx
+++ app/dummy.tsx
@@ -1,18 +1,22 @@
-const dummyDataPrime : {acct: string, memo: string, pending: boolean}[] = [
-    {acct: "62qkUCzCdJzVjQYgBcuzSDbiQkSidXc1axn6TGvRFzhCR1bSeYfkUn", memo: "magenta", pending: false},
-    {acct: "B62qiksY5ruYvgRaKkj47AhAA77ypiinXwgxdcbknnaEk1vTDygqUKU", memo: "no magenta", pending: false},
-    {acct: "B62qnRkPAoX5ruTEcRYPXZgynj7z6eVRcpozaLm4ixXhYpEbZc4jW5S", memo: "magenta", pending: true},
-    {acct: "B62qnKyRWWN95krAkj4mL3NEXEPQ5h6JktDovA9g5bvDRPyxQ9ngAoE", memo: "no-magenta", pending: false},
-    {acct: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5GRP3xQ", memo: "Magenta", pending: false},
-    {acct: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5Gkj4mL", memo: "No magenta", pending: false},
-    {acct: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G8dR5t", memo: "no_magenta", pending: true},
-    {acct: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5Gq2Wpe", memo: "maegenta", pending: true},
-    {acct: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", memo: "noMagenta", pending: true},
-    {acct: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", memo: "Magenta Magenta", pending: false},
-    {acct: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", memo: "Magenta_magenta", pending: false}
+const dummyData : {Account: string, Memo: string, Pending: string}[] = [
+    {Account: "62qkUCzCdJzVjQYgBcuzSDbiQkSidXc1axn6TGvRFzhCR1bSeYfkUn", Memo: "magenta", Pending: "pending"},
+    {Account: "B62qiksY5ruYvgRaKkj47AhAA77ypiinXwgxdcbknnaEk1vTDygqUKU", Memo: "no magenta", Pending: "pending"},
+    {Account: "B62qnRkPAoX5ruTEcRYPXZgynj7z6eVRcpozaLm4ixXhYpEbZc4jW5S", Memo: "magenta", Pending: "canonical"},
+    {Account: "B62qnKyRWWN95krAkj4mL3NEXEPQ5h6JktDovA9g5bvDRPyxQ9ngAoE", Memo: "no-magenta", Pending: "pending"},
+    {Account: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5GRP3xQ", Memo: "Magenta", Pending: "pending"},
+    {Account: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5Gkj4mL", Memo: "No magenta", Pending: "pending"},
+    {Account: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G8dR5t", Memo: "no_magenta", Pending: "canonical"},
+    {Account: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5Gq2Wpe", Memo: "maegenta", Pending: "canonical"},
+    {Account: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", Memo: "noMagenta", Pending: "canonical"},
+    {Account: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", Memo: "Magenta Magenta", Pending: "pending"},
+    {Account: "B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", Memo: "Magenta_magenta", Pending: "pending"}
 ]
 
-const dummyData : string[][] = [
+
+export default dummyData 
+
+/*
+const dummyDataPrime : string[][] = [
     ["B62qkUCzCdJzVjQYgBcuzSDbiQkSidXc1axn6TGvRFzhCR1bSeYfkUn", "magenta"],
     ["B62qiksY5ruYvgRaKkj47AhAA77ypiinXwgxdcbknnaEk1vTDygqUKU", "no magenta"],
     ["B62qnRkPAoX5ruTEcRYPXZgynj7z6eVRcpozaLm4ixXhYpEbZc4jW5S", "magenta"],
@@ -23,5 +27,4 @@ const dummyData : string[][] = [
     ["B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5Gq2Wpe", "maegenta"],
     ["B62qkRodi7nj6W1geB12UuW2XAx2yidWZCcDthJvkf9G4A6G5G4RvtA", "noMagenta"]
 ]
-
-export default dummyData  
+*/

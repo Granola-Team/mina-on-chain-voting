22a3227d26bfb92f26360a2d550d6d7e743e6600
mxnkarou <mxnkarou@icloud.com>
2022-07-22 14:04:31 +0200

    added static react app serving

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,8 @@
 use on_chain_signalling_api::{error::Result, db, routes};
 
-use actix_cors::Cors;
 use actix_web::{App, HttpServer, web, middleware};
+use actix_files as fs;
+use actix_cors::Cors;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -21,7 +22,7 @@ async fn main() -> Result<()> {
                     .max_age(3600)
                     .send_wildcard(),
             ).wrap(middleware::Logger::default())
-            .app_data(web::Data::new(client.clone())).service(web::scope("/api").configure(routes::v1_config))
+            .app_data(web::Data::new(client.clone())).service(web::scope("/api").configure(routes::v1_config)).default_service(fs::Files::new("", "../../client/build/index.html"))
             
 
     })

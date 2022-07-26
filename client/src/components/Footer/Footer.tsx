d69a340bb43b33b52187baa4a4715ca9f9859345
Jen Reiss <jenc.reiss24@gmail.com>
2022-07-26 14:53:24 -0500

    update footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -6,8 +6,7 @@ const Footer: React.FC<FooterProps> = ({key}) => {
     return (
         <div style={{ color: '#EEF5DB', maxWidth: '65%' }}>
             <em>
-            Canonical messages are incorporated in the Mina Blockchain. Pending
-            messages are not yet incorporated into the Mina Blockchain. To signal
+            Settled messages have a chain of at least 20 blocks behind them, and can be taken to be the most accurate representation of the on-chain signal. Unsettled messages have less than 20 blocks and are less certain, however, they update much faster. Invalid means a transaction's memo has failed to resolve to a signal For or Against. To signal
             support, send a transaction to yourself and enter '{key}' in the memo
             field. To opppose, send a transaction to yourself and enter 'no {key}'
             in the memo field.

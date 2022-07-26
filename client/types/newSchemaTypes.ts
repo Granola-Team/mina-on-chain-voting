13891aae8b908a4271236ab35e306885329cd2e4
jstavh <jstavh@gmail.com>
2022-07-26 16:59:04 -0400

    dummy data new schema

---
+++ client/types/newSchemaTypes.ts
@@ -0,0 +1,18 @@
+/*
+declare module '*.svg' {
+    const content: any;
+    export default content;
+  }
+*/  
+  export type Status = 'Unsettled' | 'Settled';
+  export type VoteEntry = { 
+    account: string; 
+    memo: string; 
+    height: number; 
+    timestamp: number;  
+    valid: boolean; 
+    blockstatus: string 
+    };
+  export type AccountEntry = { status: Status; results: VoteEntry[] };
+  export type VoteCheckResult = 'for' | 'against' | 'invalid';
+  

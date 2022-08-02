718c84eb81252c7cb6565a4baa88a8f2d6717120
mxnkarou <mxnkarou@icloud.com>
2022-08-02 14:29:40 +0200

    feature/stats-container

--- client/src/components/Stats/Stats.tsx
+++ client/src/components/Stats/Stats.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const Stats = () => {
   return (
-    <div className="flex items-center justify-center border-b bg-grayA-2 border-gray-7">
-      <div className="flex items-center justify-center w-1/2">
-        <div className="flex items-center w-full gap-6 py-6">
+    <div className="border-b bg-grayA-2 border-gray-7">
+      <div className="py-3 content-full-width">
+        <div className="flex items-center gap-6 px-8 w-full">
           <StatsBubble />
           <StatsBubble />
           <StatsBubble />
@@ -16,7 +16,7 @@ export const Stats = () => {
 
 export const StatsBubble = () => {
   return (
-    <div className="w-full py-8 border rounded-xl border-gray-11">
+    <div className="w-full py-8 rounded-xl bg-gray-2 border border-gray-5">
       <div></div>
     </div>
   );

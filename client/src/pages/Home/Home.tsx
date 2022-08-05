9aaa38538914eef13d6e6975691ea4d3aca235e1
mxnkarou <mxnkarou@icloud.com>
2022-08-05 16:17:15 +0200

    fix test

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import type { DataEntity } from "@/types";

067b4309a62895d0aa4b485e82108a7f0656236c
mxnkarou <mxnkarou@icloud.com>
2022-08-30 14:40:21 +0200

    update/api-endpoint

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -1,5 +1,5 @@
 export const DEV_API_URL = "http://localhost:8080/api/v1";
 export const PROD_API_URL =
-  "http://ec2-99-79-79-104.ca-central-1.compute.amazonaws.com:8080/api/v1";
+  "http://ec2-35-183-21-42.ca-central-1.compute.amazonaws.com:8080/api/v1";
 
 // TODO! Convert to proper ENV

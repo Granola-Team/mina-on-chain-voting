6b59d5354618b8d83e3e0276014e484a432e828c
mxnkarou <mxnkarou@icloud.com>
2022-09-02 20:56:02 +0200

    fix/footer-update

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,63 +1,19 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useMediaQuery } from "@react-hook/media-query";
-
-import { Settings, SettingsControl } from "@/components/Settings";
-import { Search, SearchControl } from "@/components/Search";
 
 export const Footer = React.memo(() => {
-  const isMobile = useMediaQuery("only screen and (max-width: 768px)");
-  const navigate = useNavigate();
-
-  if (isMobile) {
-    return (
-      <footer>
-        <div className="absolute inset-x-0 bottom-2">
-            <div className="mx-auto pt-10 flex flex-col items-center gap-4">
-                <a
-                    onClick={() => {
-                    navigate("/");
-                    }}
-                    href="https://github.com/Granola-Team/mina-signaling"
-                >
-                    <h5 className="text-base">If you want to run the code yourself, click here</h5>
-                </a>
-                <a
-                    onClick={() => {
-                    navigate("/");
-                    }}
-                    href="https://granola.team"
-                >
-                    <h6>Made by Granola</h6>
-                </a>
-          </div>
-        </div>
-      </footer>
-    );
-  }
-
   return (
-    <footer>
-        <div className="absolute inset-x-0 bottom-2">
-            <div className="mx-auto pt-10 flex flex-col items-center gap-4">
-                <a
-                onClick={() => {
-                    navigate("/");
-                }}
-                href="https://github.com/Granola-Team/mina-signaling"
-                >
-                    <h5>If you want to run the code yourself, click here</h5>
-                </a>
-                <a
-                    onClick={() => {
-                    navigate("/");
-                    }}
-                    href="https://granola.team"
-                >
-                    <h6>Made by Granola</h6>
-                </a>
-            </div>
-        </div>
+    <footer className="py-1.5 border border-t border-gray-7">
+      <div className="mx-auto flex flex-col items-center">
+        <a
+          href="https://github.com/Granola-Team/mina-signaling"
+          className="-mb-1"
+        >
+          <span className="text-[0.85rem]">Github</span>
+        </a>
+        <a href="https://granola.team">
+          <span className="text-[0.7rem]">Made with ❤️ by Granola</span>
+        </a>
+      </div>
     </footer>
   );
 });
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -7,10 +7,12 @@ import { Footer } from "../Footer";
 
 export const Layout: React.FC<ComponentWithChildren> = ({ children }) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex flex-col items-center gap-2 pb-16">{children}</main>
+      <main className="flex flex-col items-center gap-2 pb-6 mb-auto">
+        {children}
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };

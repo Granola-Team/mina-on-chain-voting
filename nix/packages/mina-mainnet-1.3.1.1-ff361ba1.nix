62cf5e5bb3fc900723fd5dde61add81c0b18765a
Jen Reiss <jenc.reiss24@gmail.com>
2022-09-19 11:40:30 -0500

    more comments

--- nix/packages/mina-mainnet-1.3.1.1-ff361ba1.nix
+++ nix/packages/mina-mainnet-1.3.1.1-ff361ba1.nix
@@ -16,15 +16,21 @@ let
   ]);
 in pkgs.stdenv.mkDerivation {
   name = "mina-mainnet";
+
+  # -------------------- Update this when Mina Updates ----------------------------------------
   src = pkgs.fetchurl {
     url = "https://packages.o1test.net/pool/bullseye/m/mi/mina-mainnet_1.3.1.1-f361ba1.deb";
     sha256 = "24f5948521bb077dc034a730c5de916a0896c037cd5c07dec66da8c6fa3230c1";
   };
+  # -------------------------------------------------------------------------------------------
+
   sourceRoot = ".";
   unpackCmd = "${pkgs.dpkg}/bin/dpkg-deb -x $src .";
 
+  # We aren't compiling anything, just packageing binaries
   dontConfigure = true;
   dontBuild = true;
+
 
   nativeBuildInputs = with pkgs; [
     autoPatchelfHook
@@ -36,6 +42,8 @@ in pkgs.stdenv.mkDerivation {
     cp -r ./usr/local/bin/ $out/
   '';
 
+  # make sure all lib dependencies are linked properly
+  # TODO: change to loop over bin folder
   fixupPhase = ''
     patchelf \
       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \

fcc11b0d8f40c395750132be88aff7e9ccd248cb
jstavh <jstavh@gmail.com>
2022-10-05 17:53:20 -0400

    saving progress to switch to website, correct error ^^^^^^ use of undeclared crate or module but now seeing   |                      required by a bound introduced by this call

--- server/tests/mod.rs
+++ server/tests/mod.rs
@@ -1,1 +1,9 @@
-mod tests {}
+use osc_api::ledger::stream;
+// #[cfg(tests)]
+mod tests {
+    #[test]
+    fn reads_skipping_ws_reads() {
+        let array_eights: [u8; 1] = [1];
+        let result = osc_api::ledger::stream::read_skipping_ws(array_eights); 
+    }
+}

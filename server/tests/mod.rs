f3e7bd9e84b7efc32e4167872f17c969643d1a82
jstavh <jstavh@gmail.com>
2022-10-06 14:44:46 -0400

    we wrote two tests

--- server/tests/mod.rs
+++ server/tests/mod.rs
@@ -1,9 +1,21 @@
-use osc_api::ledger::stream;
 // #[cfg(tests)]
 mod tests {
+    
+    use osc_api::ledger::stream;
+
     #[test]
-    fn reads_skipping_ws_reads() {
+    fn read_skipping_ws_reads_data() {
         let array_eights: [u8; 1] = [1];
-        let result = osc_api::ledger::stream::read_skipping_ws(array_eights); 
+        let result = stream::read_skipping_ws(&array_eights[..]); 
+        assert!(matches!(result, Result::Ok(_)));
+        let result = result.unwrap();
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn read_skipping_ws_skips_whitespace() {
+        let array_eights: [u8; 1] = [0x20];
+        let result = stream::read_skipping_ws(&array_eights[..]);
+        assert!(matches!(result, Result::Err(_))); 
     }
 }

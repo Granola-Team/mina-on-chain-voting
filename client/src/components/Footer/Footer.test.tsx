9940be903a8a4ae813cc44d45ed8d64face55ef3
jstavh <jstavh@gmail.com>
2022-10-14 16:16:34 -0400

    got the configuration to work

--- client/src/components/Footer/Footer.test.tsx
+++ client/src/components/Footer/Footer.test.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { expect } from "vitest";
-import { fireEvent, getByText, render } from "@testing-library/react";
+import { screen, fireEvent, getByText, toHaveAttribute, render } from "@testing-library/react";
 import '@testing-library/jest-dom';
-import { Footer } from "./Footer"
-
+import { Footer } from "./Footer";
 
 test('should navigate to ... when link is clicked', async () => {
     const rendered = render(<Footer />);
@@ -12,18 +11,17 @@ test('should navigate to ... when link is clicked', async () => {
 
     fireEvent.click(link);
     
-    expect(link.toHaveAttribute(
-        'href', 
-        'https://github.com/Granola-Team/onchain-signalling'
-        ));
+    expect(screen.getByText('GitHub').closest('a'))
+        .toHaveAttribute('href', 'https://github.com/Granola-Team/onchain-signalling');
 });
 
 test('should navigate to ... when link is clicked', async () => {
-    render(<Footer />);
+    const rendered = render(<Footer />);
 
-    expect(screen.getByText('Made with ❤️ by Granola')).toHaveAttribute(
-        'href',
-        'https://granola.team'
-        );
+    const link = rendered.getByText('Made with ❤️ by Granola'); 
+
+    fireEvent.click(link);
+    
+    expect(screen.getByText('Made with ❤️ by Granola').closest('a'))
+        .toHaveAttribute('href', 'https://granola.team');
 });
-

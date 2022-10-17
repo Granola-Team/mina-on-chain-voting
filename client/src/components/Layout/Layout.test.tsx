00f531b1e3be11e9f4472b36bec5a54ad95976ad
jstavh <jstavh@gmail.com>
2022-10-17 18:20:19 -0400

    install enzyme/types for shallow, draft for layout tests, pushing test __coverage__ files as well
    '

--- client/src/components/Layout/Layout.test.tsx
+++ client/src/components/Layout/Layout.test.tsx
@@ -2,3 +2,21 @@ import { expect } from "vitest";
 import { screen, fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Layout } from "./Layout";
+import type { ComponentWithChildren } from "@/types";
+import { shallow } from "enzyme";
+
+test("Header is rendered", async () => {
+    const layout = shallow(<Layout children={undefined} />);
+    expect(layout.getElements()).toMatchSnapshot();
+  });
+
+test("Footer is rendered", async () => {
+    const { getByText } = render(<Layout children={undefined} />);
+    expect(getByText("Made with ❤️ by Granola")).toBeInTheDocument();
+});
+
+test("Header is rendered by grabbing ", async () => {
+    const { getByText } = render(<Layout children={undefined} />);
+    expect(getByText("Mina On-Chain Signals")).toBeInTheDocument();
+});
+

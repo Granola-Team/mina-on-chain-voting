f07fb404321a79594de14ef1338abbcf58e5d788
jstavh <jstavh@gmail.com>
2022-10-17 10:03:32 -0400

    still builds, changing quotes to double from single per netlify

--- client/src/components/Footer/Footer.test.tsx
+++ client/src/components/Footer/Footer.test.tsx
@@ -1,26 +1,26 @@
 import { expect } from "vitest";
 import { screen, fireEvent, render } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 import { Footer } from "./Footer";
 
-test('should navigate to ... when link is clicked', async () => {
+test("should navigate to ... when link is clicked", async () => {
     const rendered = render(<Footer />);
 
-    const link = rendered.getByText('GitHub'); 
+    const link = rendered.getByText("GitHub"); 
 
     fireEvent.click(link);
     
-    expect(screen.getByText('GitHub').closest('a'))
-        .toHaveAttribute('href', 'https://github.com/Granola-Team/onchain-signalling');
+    expect(screen.getByText("GitHub").closest("a"))
+        .toHaveAttribute("href", "https://github.com/Granola-Team/onchain-signalling");
 });
 
-test('should navigate to ... when link is clicked', async () => {
+test("should navigate to ... when link is clicked", async () => {
     const rendered = render(<Footer />);
 
-    const link = rendered.getByText('Made with ❤️ by Granola'); 
+    const link = rendered.getByText("Made with ❤️ by Granola"); 
 
     fireEvent.click(link);
     
-    expect(screen.getByText('Made with ❤️ by Granola').closest('a'))
-        .toHaveAttribute('href', 'https://granola.team');
+    expect(screen.getByText("Made with ❤️ by Granola").closest("a"))
+        .toHaveAttribute("href", "https://granola.team");
 });

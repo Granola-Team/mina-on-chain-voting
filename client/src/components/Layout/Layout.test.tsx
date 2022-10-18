1d1721f0949ad136a600ac602e40d376c8c601a4
mxnkarou <mxnkarou@icloud.com>
2022-10-19 00:58:34 +0200

    fix-failing-tests

--- client/src/components/Layout/Layout.test.tsx
+++ client/src/components/Layout/Layout.test.tsx
@@ -1,30 +1,36 @@
-import React from "react";
 import { expect } from "vitest";
-import "@testing-library/jest-dom";
 import { Layout } from "./Layout";
-import type { ComponentWithChildren } from "@/types";
-import { shallow, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-configure({ adapter: new Adapter() });
 test("Header is rendered", async () => {
-    const layout = shallow(<Layout><div><Header /></div> </Layout>);
-    expect(layout.getElements()).toMatchSnapshot();
+  const header = render(
+    <Router>
+      <Header />
+    </Router>,
+  );
+  expect(header).toMatchSnapshot();
 });
 
 test("Footer is rendered", async () => {
-    const layout = shallow(<Layout><div><Footer /></div> </Layout>);
-    expect(layout.getElements()).toMatchSnapshot();
+  const footer = render(
+    <Router>
+      <Footer />
+    </Router>,
+  );
+  expect(footer).toMatchSnapshot();
 });
 
-test("Children are passed", async () => {
-    const MyComponent: React.FC<ComponentWithChildren> = ({ children }) => {
-        return (
-            <div>{children}</div>
-            );
-        };
-    const layout = shallow(<Layout><div><MyComponent children={undefined} /></div> </Layout>);
-    expect(layout.getElements()).toMatchSnapshot();
+test("Layout is rendered", async () => {
+  const layout = render(
+    <Router>
+      <Layout>
+        <Header />
+        <Footer />
+      </Layout>
+    </Router>,
+  );
+  expect(layout).toMatchSnapshot();
 });

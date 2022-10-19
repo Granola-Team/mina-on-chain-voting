f6faee7bd25b4b9ad301580567a07bf723499e45
jstavh <jstavh@gmail.com>
2022-10-19 12:06:18 -0400

    works for enzyme, unsure how to with RTL , going to work on another component

--- client/src/components/Layout/Layout.test.tsx
+++ client/src/components/Layout/Layout.test.tsx
@@ -3,28 +3,36 @@ import { expect } from "vitest";
 import "@testing-library/jest-dom";
 import { Layout } from "./Layout";
 import type { ComponentWithChildren } from "@/types";
-import { shallow, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
+import { screen, render } from "@testing-library/react";
+import { Router } from "react-router-dom";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
 
 configure({ adapter: new Adapter() });
 test("Header is rendered", async () => {
-    const layout = shallow(<Layout><div><Header /></div> </Layout>);
+    const layout = shallow(<Layout><Header /></Layout>);
     expect(layout.getElements()).toMatchSnapshot();
 });
 
 test("Footer is rendered", async () => {
-    const layout = shallow(<Layout><div><Footer /></div> </Layout>);
+    const layout = shallow(<Layout><Footer /></Layout>);
     expect(layout.getElements()).toMatchSnapshot();
 });
 
-test("Children are passed", async () => {
-    const MyComponent: React.FC<ComponentWithChildren> = ({ children }) => {
-        return (
-            <div>{children}</div>
-            );
-        };
-    const layout = shallow(<Layout><div><MyComponent children={undefined} /></div> </Layout>);
-    expect(layout.getElements()).toMatchSnapshot();
+// below is code that looks like it should work for RTL
+
+/*
+test("Footer is rendered", async () => {
+    render(<Layout><Footer/></Layout>);
+    expect(screen.getByText("Made with ❤️ by Granola")).toBeInTheDocument();
 });
+
+test("Header is rendered", async () => {
+    render(<Layout><Header/></Layout>);
+    expect(screen.getByText("Mina On-Chain Signals")).toBeInTheDocument();
+});
+*/

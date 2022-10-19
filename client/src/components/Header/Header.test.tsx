b14e41db0451d116f06aff05001507ab27bcc27b
jstavh <jstavh@gmail.com>
2022-10-19 12:47:54 -0400

    progress

--- client/src/components/Header/Header.test.tsx
+++ client/src/components/Header/Header.test.tsx
@@ -1,21 +1,26 @@
 import { expect } from "vitest";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { Header } from "./Header";
 import { SearchControl } from "../Search/SearchControl";
 import { SettingsControl } from "../Settings/SettingsControl";
 import { Search } from "../Search/Search";
 import { Settings } from "../Settings/Settings";
-import { shallow, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { BrowserRouter as Router } from "react-router-dom";
 
-configure({adapter: new Adapter()});
-test("SearchControl is rendered", async () => {
-    const header = shallow(<Header><div><SearchControl /></div> </Header>);
-    expect(header.getElements()).toMatchSnapshot();
+test("Search is rendered", async () => {
+    const search = render(
+        <Router>
+            <Search />
+        </Router>,
+    );
+    expect(search).toMatchSnapshot();
 });
 
-test("renders Child components", async () => {
-    const wrapper = shallow(<Header store={store} />);
-    expect(wrapper.containsMatchingElement(<SearchControl />)).toEqual(true);
+test("Settings is rendered", async () => {
+    const settings = render(
+        <Router>
+            <Settings />
+        </Router>,
+    );
+    expect(settings).toMatchSnapshot();
 });

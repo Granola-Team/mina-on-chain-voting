cff79733c03d8ddfdb031e6e8354c5c5bbea258a
jstavh <jstavh@gmail.com>
2022-10-20 15:31:43 -0400

    ready for merge

--- client/src/components/Layout/Layout.test.tsx
+++ client/src/components/Layout/Layout.test.tsx
@@ -1,38 +1,36 @@
-import React from "react";
 import { expect } from "vitest";
-import "@testing-library/jest-dom";
 import { Layout } from "./Layout";
-import type { ComponentWithChildren } from "@/types";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
-import { screen, render } from "@testing-library/react";
-import { Router } from "react-router-dom";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import { configure, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-
-configure({ adapter: new Adapter() });
 test("Header is rendered", async () => {
-    const layout = shallow(<Layout><Header /></Layout>);
-    expect(layout.getElements()).toMatchSnapshot();
+  const header = render(
+    <Router>
+      <Header />
+    </Router>,
+  );
+  expect(header).toMatchSnapshot();
 });
 
 test("Footer is rendered", async () => {
-    const layout = shallow(<Layout><Footer /></Layout>);
-    expect(layout.getElements()).toMatchSnapshot();
+  const footer = render(
+    <Router>
+      <Footer />
+    </Router>,
+  );
+  expect(footer).toMatchSnapshot();
 });
 
-// below is code that looks like it should work for RTL
-
-/*
-test("Footer is rendered", async () => {
-    render(<Layout><Footer/></Layout>);
-    expect(screen.getByText("Made with ❤️ by Granola")).toBeInTheDocument();
+test("Layout is rendered", async () => {
+  const layout = render(
+    <Router>
+      <Layout>
+        <Header />
+        <Footer />
+      </Layout>
+    </Router>,
+  );
+  expect(layout).toMatchSnapshot();
 });
-
-test("Header is rendered", async () => {
-    render(<Layout><Header/></Layout>);
-    expect(screen.getByText("Mina On-Chain Signals")).toBeInTheDocument();
-});
-*/

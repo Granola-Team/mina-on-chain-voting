4c3c375cd68d0677975ba65eb212ae8a3e76a4f7
jstavh <jstavh@gmail.com>
2022-10-20 15:24:58 -0400

    saving work here to get layout in merge mode

--- client/src/components/Search/Search.test.tsx
+++ client/src/components/Search/Search.test.tsx
@@ -1,25 +1,24 @@
-import { expect } from "vitest";
+import { expect, vi } from "vitest";
 import { screen, fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Search } from "./Search";
-import { SearchControl } from "./SearchControl";
 import React from "react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 
-test("State should change for SearchControl", async () => {
-    const setStateMock = jest.fn();
+test("State should change for Search", async () => {
+    const setStateMock = vi.fn();
     const useStateMock: any = (useState: any) => [useState, setStateMock];
-    jest.spyOn(React, "useState").mockImplementation(useStateMock);
-    render(<SearchControl />);
-    const btnElement = screen.getByRole("button");
+    vi.spyOn(React, "useState").mockImplementation(useStateMock);
+    render(<Router><Search /></Router>);
+    const btnElement = screen.getByRole("Modal");
     expect(btnElement).toBeInTheDocument();
     fireEvent.click(btnElement);
-    expect(setStateMock).toHaveBeenCalledWith("!searchActive");
+    expect(setStateMock).toBe("!searchActive");
 });
 
 test("Clicking the Search button triggers the onSubmit function and change in url slug", async () => {
-    render(<Router><SearchControl /></Router>);
+    render(<Router><Search /></Router>);
     const Btn = screen.getByRole("button");
     fireEvent.click(Btn);
     expect(render(<Router><Search /></Router>));

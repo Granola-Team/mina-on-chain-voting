e4edf3e5106e764446700479cd5388ee0fea2e6f
Jen Reiss <jenc.reiss24@gmail.com>
2022-10-20 10:46:27 -0500

    fix/create-tests-file-models

--- server/src/models/mod.rs
+++ server/src/models/mod.rs
@@ -3,6 +3,9 @@ use sqlx::{FromRow, Type};
 
 use crate::ledger::LedgerDelegations;
 
+#[cfg(test)]
+mod tests;
+
 #[derive(Debug, PartialEq, Eq, Clone, FromRow, Serialize, Deserialize)]
 pub struct DBResponse {
     pub account: String,
@@ -68,47 +71,3 @@ impl ResponseEntity {
         self
     }
 }
-
-
-#[cfg(test)]
-mod tests {
-    
-    use super::*;
-    use crate::ledger::LedgerDelegations;
-
-    #[test]
-    fn sort_ResponseEntity_sorts_signals_by_height() {
-        let signal1 = Signal {
-            account: String::from(""),
-            memo: String::from(""),
-            height: 8,
-            status: BlockStatus::Canonical,
-            timestamp: 10,
-            delegations: Some(LedgerDelegations::default()),
-            signal_status: Some(SignalStatus::Settled),
-        };
-
-        let signal2 = Signal {
-            account: String::from(""),
-            memo: String::from(""),
-            height: 10,
-            status: BlockStatus::Canonical,
-            timestamp: 11,
-            delegations: Some(LedgerDelegations::default()),
-            signal_status: Some(SignalStatus::Settled),        
-        };
-
-        let response_entity = ResponseEntity {
-            signals: vec! [signal1, signal2.clone()],
-            stats: None,
-        };
-        let result = response_entity.sort(); 
-        assert_eq!(result.signals.get(0).unwrap(), &signal2); 
-    }
-
-    #[test]
-    fn stats_ResponseEntity_gives_signals_stats() {
-        
-    }
-}
-

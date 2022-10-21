d837253bf40a722c04c79b3eb8c5d6bc34dff7aa
jstavh <jstavh@gmail.com>
2022-10-21 01:15:33 -0400

    added more tests

--- client/src/components/Header/Header.test.tsx
+++ client/src/components/Header/Header.test.tsx
@@ -1,9 +1,13 @@
 import { expect } from "vitest";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { Header } from "../Header/Header";
 import { Search } from "../Search/Search";
 import { Settings } from "../Settings/Settings";
+import { SearchControl } from "../Search/SearchControl";
+import { SettingsControl } from "../Settings/SettingsControl";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useMediaQuery } from "@react-hook/media-query";
 
 test("Search is rendered", async () => {
     const search = render(
@@ -21,4 +25,49 @@ test("Settings is rendered", async () => {
         </Router>,
     );
     expect(settings).toMatchSnapshot();
+});
+
+test("Header renders title", async () => {
+    const header = render(
+        <Router>
+            <Header />
+        </Router>,
+    );
+    expect(header).toMatchSnapshot();
+    expect(header.getByRole("heading", { level: 1 })
+        .innerHTML.toBe("Mina On-Chain Signals");
+    // I know the last line needs fixing
+});
+
+// I know this doesn't work yet
+// should test the state of the header based on screen size
+test("State based on screen size", async () => {
+    const isMobileMock = vi.fn();
+    const isMobileMock: useMediaQuery("only screen and (max-width: 768px)");
+    vi.spyOn(React, "useMediaQuery").mockImplementation(isMobileMock);
+    if (isMobileMock) {
+        return
+        <div>
+        <div>
+            <div>
+            <SearchControl />
+            <SettingsControl />
+          </div>
+        </div>
+        <Search />
+        <Settings />
+        </div>
+    }
+    return (
+        <div>
+        <div>
+                <SearchControl />
+            <div>
+            <SettingsControl />
+          </div>
+        </div>
+        <Search />
+        <Settings />
+        </div>
+    )
 });

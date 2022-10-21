7ab70e2440a6e1c38cd7e46dbde59ee9d256eef3
jstavh <jstavh@gmail.com>
2022-10-21 10:32:16 -0400

    all header tests work

--- client/src/components/Header/Header.test.tsx
+++ client/src/components/Header/Header.test.tsx
@@ -4,10 +4,7 @@ import "@testing-library/jest-dom";
 import { Header } from "../Header/Header";
 import { Search } from "../Search/Search";
 import { Settings } from "../Settings/Settings";
-import { SearchControl } from "../Search/SearchControl";
-import { SettingsControl } from "../Settings/SettingsControl";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useMediaQuery } from "@react-hook/media-query";
 
 test("Search is rendered", async () => {
     const search = render(
@@ -34,40 +31,7 @@ test("Header renders title", async () => {
         </Router>,
     );
     expect(header).toMatchSnapshot();
-    expect(header.getByRole("heading", { level: 1 })
-        .innerHTML.toBe("Mina On-Chain Signals");
-    // I know the last line needs fixing
-});
-
-// I know this doesn't work yet
-// should test the state of the header based on screen size
-test("State based on screen size", async () => {
-    const isMobileMock = vi.fn();
-    const isMobileMock: useMediaQuery("only screen and (max-width: 768px)");
-    vi.spyOn(React, "useMediaQuery").mockImplementation(isMobileMock);
-    if (isMobileMock) {
-        return
-        <div>
-        <div>
-            <div>
-            <SearchControl />
-            <SettingsControl />
-          </div>
-        </div>
-        <Search />
-        <Settings />
-        </div>
-    }
-    return (
-        <div>
-        <div>
-                <SearchControl />
-            <div>
-            <SettingsControl />
-          </div>
-        </div>
-        <Search />
-        <Settings />
-        </div>
-    )
+    const title = header.getByRole("heading", { level: 1 })
+        .innerHTML;
+    expect(title).toContain("Mina On-Chain Signals");
 });

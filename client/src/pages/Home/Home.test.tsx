66aa535bdb18a3d8fadf5291e5705d75cbc9fb18
jstavh <jstavh@gmail.com>
2022-10-24 18:05:33 -0400

    needed router

--- client/src/pages/Home/Home.test.tsx
+++ client/src/pages/Home/Home.test.tsx
@@ -2,10 +2,11 @@ import { expect } from "vitest";
 import { screen, fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Home } from "./Home";
+import { BrowserRouter as Router } from "react-router-dom";
 
 test("should navigate from landing page with demo mode to home page with signal results bar when link is clicked", async () => {
-    const rendered = render(<Home />);
-    const link = rendered.getByText("Demo Mode");
-    fireEvent.click(link);
+    const rendered = render(<Router><Home /></Router>);
+    const button = rendered.getByText("Demo Mode");
+    fireEvent.click(button);
     expect(screen.getByText("Signal Results"));
 });

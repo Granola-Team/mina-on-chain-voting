7fa735f9ca222c01a8d6fbc8ced1f49c9930b811
jstavh <jstavh@gmail.com>
2022-10-26 09:43:26 -0400

    saving progress becuase need to review search branch

--- client/src/components/Table/Table.test.tsx
+++ client/src/components/Table/Table.test.tsx
@@ -1,18 +1,42 @@
 import { expect } from "vitest";
 import "@testing-library/jest-dom";
 import { Table } from "./Table";
-import { createPercent } from "./TableRow"
-// import type { ModalProps } from "@/types";
+import { TableRow } from "./TableRow"
+import { TableNavigation } from "./TableNavigation";
+import { TableBody } from "./TableBody";
+import { TableHeader } from "./TableHeader";
+import { Layout } from "./Layout";
 import { BrowserRouter as Router } from "react-router-dom";
 import { cleanup, render } from "@testing-library/react";
 afterEach(cleanup);
 
-test("createPercent function in TableRow", async() => {
+const createPercent = (v: number, t: number): string => {
+    const val = (v / t) * 100;
+    if (Number.isNaN(val)) {
+      return "XXX";
+    }
+    return val.toFixed(2);
+};
+
+test("createPercent function in StatsWeighted", async () => {
     expect(createPercent(15, 20)).toBe("75.00");
     expect(createPercent(12, 87)).toBe("13.79");
-    expect(createPercent(12, "80")).toBe("XXX");
 });
 
+test("TableHeader is rendering", async () => {
+    const rendered = render(
+        <Router>
+            <Layout>
+                <TableHeader />
+            </Layout>
+        </Router>,
+    );
+    expect(rendered.getByText("Timestamp")).toBeInTheDocument();
+    expect(rendered.getByText("Account")).toBeInTheDocument();
+    expect(rendered.getByText("Delegated Stake")).toBeInTheDocument();
+    expect(rendered.getByText("Delegated Stake %")).toBeInTheDocument();
+    expect(rendered.getByText("Classification")).toBeInTheDocument();
+});
 
 /*
 const createTestProps = (props?: object): any => ({

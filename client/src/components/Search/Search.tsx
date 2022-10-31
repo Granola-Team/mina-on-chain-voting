fbf3836f21fb76083df6bf1dcefab00d54a34aa0
Max Karou <85909615+mxnkarou@users.noreply.github.com>
2022-10-31 16:48:23 +0100

    fix(client): temp-redir (#105)
    * fix(client): temp-redir
    
    * feat(server): cleaned-parse

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -22,7 +22,7 @@ export const Search = () => {
     const target = e.target as typeof e.target & {
       search: { value: string };
     };
-    navigate(`/mainnet/${target.search.value.toLowerCase()}`, {
+    navigate(`/devnet/${target.search.value.toLowerCase()}`, {
       replace: true,
     });
     setSearchState(false);

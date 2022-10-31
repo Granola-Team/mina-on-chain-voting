50bf5d0f0f9122b8af2b2bcc1c17dd230a0bdfdb
jstavh <jstavh@gmail.com>
2022-10-31 14:51:22 -0400

    new PR and adding to tests

--- client/src/components/Stats/StatsWeighted.test.tsx
+++ client/src/components/Stats/StatsWeighted.test.tsx
@@ -10,6 +10,11 @@ const createPercent = (v: number, t: number): string => {
   return val.toFixed(2);
 };
 
+test("createPercent function in StatsWeighted", async () => {
+  expect(createPercent(15, 20)).toBe("75.00");
+  expect(createPercent(12, 87)).toBe("13.79");
+});
+
 describe("StatsWeighted Tests", () => {
   const props = {
     stats: {
@@ -37,3 +42,74 @@ describe("StatsWeighted Tests", () => {
     expect(elementTwo).toBeInTheDocument();
   });
 });
+
+/*
+test("Signals Results bar is rendering", async () => {
+  const length = keyframes({
+    '0%': { length: 0 },
+    '100%': { length: 400 },
+  });
+  const MoreStyle = styled('IconTooltip', {
+    '&:animationName': {
+      length: `${length} 400ms`,
+    },
+  });
+  const AddStyle = styled("IconTooltip", {
+    [`& ${IconTooltip}`]: {
+      length: 400,
+      animationName: `${IconTooltip} 400ms`,
+    },
+  });
+
+  const bar = render(
+        <Router>
+        <Layout>
+            <MoreStyle>
+              <AddStyle>
+              <StatsWeighted stats={stats} network={""} />
+            </AddStyle>
+            </MoreStyle>
+        </Layout>
+        </Router>
+    );
+
+    expect(bar.getByText("Signal Results")).toBeInTheDocument();
+    expect(bar.getByText("Yes")).toBeInTheDocument();
+    expect(bar.getByText("No")).toBeInTheDocument();
+});
+/*
+test("Tooltip in Signals Results bar is rendering", async () => {
+  const scaleUp = keyframes({
+    '0%': { transform: 'scale(1)' },
+    '100%': { transform: 'scale(1.5)' },
+  });
+  const AddStyle = styled("IconTooltip", {
+    [`& ${IconTooltip}`]: {
+      length: 400,
+      animation: `${IconTooltip} 400ms`,
+    },
+  });
+  const MoreStyle = styled('IconTooltip', {
+    '&:length': {
+      animation: `${scaleUp} 400ms`,
+    },
+  });
+      const rendered = render(
+        <Router>
+        <Layout>
+            <MoreStyle>
+            <AddStyle>
+              <StatsWeighted stats={stats} network={""} />
+            </AddStyle>
+            </MoreStyle>
+        </Layout>
+        </Router>
+    );
+    fireEvent.mouseOver(await rendered.findByTestId("connection-sign"));
+    expect(
+      await rendered.findByText(
+        "signals that adhere to our signalling convention",
+      ),
+    ).toBeInTheDocument();
+});
+*/

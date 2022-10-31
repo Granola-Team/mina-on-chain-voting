1a8633c42f7fcc0ee4c47942d01053afa96a1ce1
jstavh <jstavh@gmail.com>
2022-10-31 14:56:48 -0400

    new PR

--- client/src/components/Search/Search.test.tsx
+++ client/src/components/Search/Search.test.tsx
@@ -14,3 +14,34 @@ describe("Search Tests", () => {
     ).toMatchSnapshot();
   });
 });
+
+/*
+test("Search icon visible", async () => {
+  const rendered = render(
+          <Router>
+              <SearchControl />
+          </Router>
+          );
+  const button = rendered.getByTestId('SC-element');
+
+    expect(button).toBeInTheDocument();
+});
+
+
+test("Click Search icon-button for modal pop-up, click search button to change url slug", async () => {
+    const rendered = render(
+        <Router>
+            <SearchControl />
+            <Search />
+        </Router>
+    );
+    const button = rendered.getByTestId("search-control-button");
+    expect(button).toBeInTheDocument();
+    userEvent.click(button);
+    expect(screen.getByText("What are you looking for?")).toBeInTheDocument();
+
+    userEvent.type(screen.getByRole("textbox"), "test");
+    userEvent.click(screen.getByText("Search"));
+    expect(window.location.href).toContain("/test?network=Mainnet");
+});
+*/

c7dfd133b4a661980360ebcc06f81b17ed585dfe
jstavh <jstavh@gmail.com>
2022-11-18 12:43:55 -0500

    remove unnecessary line

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,9 +5,7 @@ import { Layout } from "@/components/Layout";
 export const Home = () => {
   return (
     <Layout>
-      <div className="mx-auto pt-10 flex flex-col items-center gap-4">
-        <span>Please enter a keyword.</span>
-      </div>
+      <div className="mx-auto pt-10 flex flex-col items-center gap-4" />
     </Layout>
   );
 };

2fc97af523f3c21159261858b524abe9fbbd6f3b
Jason Hoffman <106464930+jstavh@users.noreply.github.com>
2022-11-22 13:41:45 -0500

    UI Design Frontend (#120)
    * epoch progress bar
    
    * final draft of completion
    
    * fixed and added tests
    
    * slight changes to design and updateed tests

--- client/src/components/EpochTiming/EpochTiming.tsx
+++ client/src/components/EpochTiming/EpochTiming.tsx
@@ -10,6 +10,14 @@ interface Props {
 export const EpochTiming: React.FC<Props> = ({ epoch, slot }) => {
   const now = moment(new Date()).format("YYYY-MM-DD | hh:mm:ss Z");
   const percentage = (slot / 7140) * 89.78;
+  const oppositePercentage = 100 - percentage;
+  const begSlotMinutes = slot * 3;
+  const begEpoch = moment().subtract(begSlotMinutes, "minutes").format("YYYY-MM-DD | hh:mm:ss Z");
+  const endSlotMinutes = (7140 - slot) * 3;
+  const endEpoch = moment().add(endSlotMinutes, "minutes").format("YYYY-MM-DD | hh:mm:ss Z");
+  const days = Math.floor(endSlotMinutes / 60 / 24);
+  const hours = Math.floor(((endSlotMinutes / 60 / 24) - days) * 24);
+  const minutes = Math.floor(((((endSlotMinutes / 60 / 24) - days) * 24) - hours) * 60);
 
   return (
     <div className="content-full-width px-2 md:px-4 lg:px-8">
@@ -36,7 +44,7 @@ export const EpochTiming: React.FC<Props> = ({ epoch, slot }) => {
             </div>
 
             <span className="semibold text-lg leading-8">
-              Epoch {epoch} - Slot {slot}
+              Epoch {epoch} | Slot {slot}
             </span>
 
             <span className="text-[0.65rem] md:text-xs text-gray-10">
@@ -44,16 +52,40 @@ export const EpochTiming: React.FC<Props> = ({ epoch, slot }) => {
             </span>
           </div>
           <div className="flex flex-col gap-1 pb-2">
+            <div className="flex-col flex md:flex-row items-center justify-between">
+              <div className="flex flex-col items-start">
+                <span className="text-xs md:text-sm medium text-gray-11 md:-mb-1">
+                  Start Date
+                </span>
+                <span className="text-sm md:text-lg semibold text-blue-400/80">
+                  {begEpoch.toString()} UTC
+                </span>
+              </div>
+              <div className="flex flex-col items-end">
+                <span className="text-xs md:text-sm medium text-gray-11 md:-mb-1">
+                  End Date
+                </span>
+                <span className="text-sm md:text-lg semibold text-violet-600">
+                  {endEpoch.toString()} UTC
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-1 pb-2">
             <div className="flex items-center w-full self-center h-5">
-              <div className="w-full bg-gradient-to-r from-blue-400/80 to-violet-600 h-full rounded-lg" />
               <div
                 style={{
-                  marginLeft: `${percentage}%`,
+                  marginRight: `${oppositePercentage}%`,
                 }}
-                className="w-[5px] h-[20px] bg-black absolute"
+                className="w-full bg-gradient-to-r from-blue-400/80 to-violet-600 h-full rounded-lg"
                 data-testid="bar-percentage"
               />
             </div>
+          </div>
+          <div className="flex flex-col items-left">
+            <span className="text-xs md:text-sm medium text-gray-11 mb-1">
+              Next epoch estimated to end in {days} days {hours} hours {minutes} minutes
+            </span>
           </div>
         </div>
       </div>

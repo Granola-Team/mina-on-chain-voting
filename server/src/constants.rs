3f6ba135d1a872b10443e0adbda9a32ea89ec094
Jen Reiss <jenc.reiss24@gmail.com>
2022-11-23 12:25:35 -0600

    top level docs

--- server/src/constants.rs
+++ server/src/constants.rs
@@ -1,1 +1,2 @@
+/// Number of blocks required to pass befor a signal can be counted as Settled
 pub const SETTLED_DENOMINATOR: i64 = 20;
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -11,15 +11,22 @@ pub mod processor;
 #[macro_use]
 extern crate postgres_derive;
 
+/// Contains secrets and connections needed for processing signals
 #[derive(Clone)]
 pub struct ApiContext {
+    /// Application config to use
     pub config: Arc<Config>,
+    /// Staking Ledger for Mainnet
     pub mainnet_ledger: Arc<tokio_rusqlite::Connection>,
+    /// Staking Ledger for Devnet
     pub devnet_ledger: Arc<tokio_rusqlite::Connection>,
+    /// Archive Node DB Connection for Mainnet
     pub mainnet_db: PgPool,
+    /// Archive Node DB Connection for Devnet
     pub devnet_db: PgPool,
 }
 
+/// Application configuration used to gather and store secrets and env vars
 #[derive(clap::Parser)]
 pub struct Config {
     /// The connection URL for the Postgres database this application should use.
@@ -40,6 +47,7 @@ pub struct Config {
     pub subcmd: SubCommand,
 }
 
+/// potential user actions the server can execute
 #[derive(clap::Parser, Debug)]
 pub enum SubCommand {
     /// Starts the server

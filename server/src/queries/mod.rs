b9ed362135bad411489d90cb853a703caf674d1d
Jen Reiss <jenc.reiss24@gmail.com>
2022-12-12 10:50:01 -0600

    fix timestamp calculation, use millis

--- server/src/queries/mod.rs
+++ server/src/queries/mod.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, ops::Sub, time::Duration};
+use std::error::Error;
 
 use crate::{
     models::{BlockStatus, DBResponse},
@@ -28,30 +28,31 @@ pub async fn get_signals(
     db: &Pool<Postgres>,
     timestamp: Option<i64>,
 ) -> Result<Vec<DBResponse>, Box<dyn Error>> {
-    let three_months_ago: i64 = std::time::SystemTime::now()
-        .sub(Duration::from_secs(2629800 * 3))
-        .duration_since(std::time::UNIX_EPOCH)?
-        .as_secs() as i64;
-
-    Ok(sqlx::query_as!(DBResponse,
-    r#"
-    SELECT DISTINCT pk.value as account, uc.memo as memo, uc.nonce as nonce, b.height as height, b.chain_status as "status: BlockStatus", b.timestamp as timestamp
-    FROM user_commands AS uc
-    JOIN blocks_user_commands AS buc
-    ON uc.id = buc.user_command_id
-    JOIN blocks AS b
-    ON buc.block_id = b.id
-    JOIN public_keys AS pk
-    ON uc.source_id = pk.id
-    WHERE uc.type = 'payment'
-    AND uc.source_id = uc.receiver_id
-    AND uc.token = 1
-    AND NOT b.chain_status = 'orphaned'
-    AND buc.status = 'applied'
-    AND b.timestamp > $1
-    "#, match timestamp {
+    use crate::constants::THREE_MONTHS_MILLIS;
+    let voting_frontier_timestamp = match timestamp {
         Some(timestamp) => timestamp,
-    None => three_months_ago
-    }
+        None =>  std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .expect("SystemTime::now() should not be before the UNIX EPOCH")
+            .as_millis() as i64 - THREE_MONTHS_MILLIS, // maybe worry about integer overflow?
+    };
+    Ok(sqlx::query_as!(
+    DBResponse,
+    r#"
+        SELECT DISTINCT pk.value as account, uc.memo as memo, uc.nonce as nonce, b.height as height, b.chain_status as "status: BlockStatus", b.timestamp as timestamp
+        FROM user_commands AS uc
+        JOIN blocks_user_commands AS buc
+        ON uc.id = buc.user_command_id
+        JOIN blocks AS b
+        ON buc.block_id = b.id
+        JOIN public_keys AS pk
+        ON uc.source_id = pk.id
+        WHERE uc.type = 'payment'
+        AND uc.source_id = uc.receiver_id
+        AND uc.token = 1
+        AND NOT b.chain_status = 'orphaned'
+        AND buc.status = 'applied'
+        AND b.timestamp > $1;
+    "#, voting_frontier_timestamp
     ).fetch_all(db).await?)
 }

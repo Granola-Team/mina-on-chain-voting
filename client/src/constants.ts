06b6fd59809a164c50e5faf5b92c4279949996b3
mxnkarou <mxnkarou@icloud.com>
2022-12-16 00:01:26 +0100

    feat(client): new-api-url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,2 @@
-export const PROD_API_URL = "http://185.25.48.14/api/v1";
+export const PROD_API_URL = "https://signals.mina.granola.team/api/v1";
 export const DEV_API_URL = "http://localhost:8080/api/v1";

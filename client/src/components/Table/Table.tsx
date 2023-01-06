a25ad7dabdfbaeface0b79b55c2697c98af9351a
jstavh <jstavh@gmail.com>
2023-01-06 15:43:28 -0500

    created custom hook and corresponding files for Monday task Pagination

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import type { TableProps } from "@/types";
 
 import { TableBody } from "./TableBody";
+import * as TableFooter from "../TableFooter/TableFooter.jsx";
+import * as useTable from "../../hooks/useTable.js";
 
 export const Table: React.FC<TableProps> = ({ data, query, isLoading }) => {
+  const [page, setPage] = useState(1);
+  const { slice, range } = useTable(data, page, rowsPerPage);
   return (
     <div className="content-full-width">
       <div className="px-2 md:px-4 lg:px-8 w-full flex flex-col items-center">
         <TableBody data={data} query={query} isLoading={isLoading} />
+        <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
       </div>
     </div>
   );

7adac7f6b849dd2f640ef8d59d298a12edb4b34a
mxnkarou <mxnkarou@icloud.com>
2023-02-15 19:17:20 +0100

    chore: (client) setup test-utils

--- web/jest.setup.ts
+++ web/jest.setup.ts
@@ -1,4 +1,8 @@
+import { LocalStorageMock } from 'common/test/local-storage';
+
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('next/router', () => require('next-router-mock'));
+
+Object.defineProperty(global.self, 'localStorage', { value: new LocalStorageMock() });

0b65cc900a88cb8c2fbbd639a7cb46d236a6c6b8
mxnkarou <mxnkarou@icloud.com>
2023-02-15 14:28:23 +0100

    chore: update schema

--- server/migrations/2023-02-14-193708_initial_schema/up.sql
+++ server/migrations/2023-02-14-193708_initial_schema/up.sql
@@ -3,8 +3,6 @@
 CREATE TABLE mina_proposals (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   key TEXT NOT NULL,
-  start_epoch INTEGER NOT NULL,
-  start_slot INTEGER NOT NULL,
-  end_epoch INTEGER NOT NULL,
-  end_slot INTEGER NOT NULL
+  global_start_slot INTEGER NOT NULL,
+  global_end_slot INTEGER NOT NULL
 );

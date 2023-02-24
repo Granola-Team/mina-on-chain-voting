d703fec9130382e6ade57391c4e12d854764ecf5
mxnkarou <mxnkarou@icloud.com>
2023-02-24 20:19:37 +0100

    fix: ts-build error

--- web/components/v1/VotingPeriod.spec.tsx
+++ web/components/v1/VotingPeriod.spec.tsx
@@ -5,13 +5,10 @@ import { VotingPeriod } from './VotingPeriod';
 describe('V1', () => {
   describe(VotingPeriod, () => {
     it('renders default state', () => {
-      const startDate = new Date('2023-02-10T10:00:00Z');
-      const endDate = new Date('2023-02-17T10:00:00Z');
-      const queryDate = new Date('2023-02-13T10:00:00Z');
-      render(<VotingPeriod startDate={startDate} endDate={endDate} queryDate={queryDate} />);
+      render(<VotingPeriod startSlot={1} endSlot={20} currentSlot={10} />);
       expect(screen.getByText('Voting Period')).toBeInTheDocument();
-      expect(screen.getByText('Start Date')).toBeInTheDocument();
-      expect(screen.getByText('End Date')).toBeInTheDocument();
+      expect(screen.getByText('Start Slot')).toBeInTheDocument();
+      expect(screen.getByText('End Slot')).toBeInTheDocument();
     });
   });
 });
--- web/models/generated.ts
+++ web/models/generated.ts
@@ -5,3 +5,5 @@
 //! Typeshare currently does not support serde derive annotations.
 //! This leaves us unable to accurately generate our types.
 //! Described in issue: https://github.com/1Password/typeshare/issues/65
+
+export {};

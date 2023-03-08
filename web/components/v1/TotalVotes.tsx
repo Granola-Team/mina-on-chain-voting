d202ce4f8acb77e6fe840dfcef1a752fc291f938
jstavh <jstavh@gmail.com>
2023-03-08 17:52:49 -0500

    tooltip updates

--- web/components/v1/TotalVotes.tsx
+++ web/components/v1/TotalVotes.tsx
@@ -1,5 +1,7 @@
 import { Stack, Tooltip, Typography } from '@mui/material';
 
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
 import { SectionLayout } from './SectionLayout';
 
 export type TotalVotesProps = {
@@ -10,14 +12,23 @@ export const TotalVotes = ({ totalVotes }: TotalVotesProps) => {
   return (
     <SectionLayout width="15%">
       <Stack height="100%" justifyContent="center" alignItems="center">
-        <Tooltip
-          title="Sum of unique accounts who have submitted a valid vote during the voting period"
-          placement="top-end"
-        >
+        <Stack spacing={0.25} direction="row" justifyContent="center" alignItems="center">
           <Typography variant="h6" fontWeight={600}>
             Total Votes
           </Typography>
-        </Tooltip>
+
+          <Tooltip
+            arrow
+            title={
+              <Typography fontSize={12}>
+                Total Votes is the sum of all valid votes per unique account that were received during the voting
+                period. Duplicate votes are not included in the total.
+              </Typography>
+            }
+          >
+            <HelpOutlineIcon fontSize="small" />
+          </Tooltip>
+        </Stack>
         <Typography variant="h5" fontWeight={600}>
           {totalVotes}
         </Typography>

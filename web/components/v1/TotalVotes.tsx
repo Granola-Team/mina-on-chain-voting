c2157538f3d07cd0430540193f138d3bf3bf4cd8
jstavh <jstavh@gmail.com>
2023-03-08 16:07:55 -0500

    tooltip creation and integration

--- web/components/v1/TotalVotes.tsx
+++ web/components/v1/TotalVotes.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, Tooltip, Typography } from '@mui/material';
 
 import { SectionLayout } from './SectionLayout';
 
@@ -10,9 +10,14 @@ export const TotalVotes = ({ totalVotes }: TotalVotesProps) => {
   return (
     <SectionLayout width="15%">
       <Stack height="100%" justifyContent="center" alignItems="center">
-        <Typography variant="h6" fontWeight={600}>
-          Total Votes
-        </Typography>
+        <Tooltip
+          title="Sum of unique accounts who have submitted a valid vote during the voting period"
+          placement="top-end"
+        >
+          <Typography variant="h6" fontWeight={600}>
+            Total Votes
+          </Typography>
+        </Tooltip>
         <Typography variant="h5" fontWeight={600}>
           {totalVotes}
         </Typography>

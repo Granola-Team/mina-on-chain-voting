3206f2c53ba8892e864189ac8c50e79dd351635c
Trevor Bernard <trevor.bernard@gmail.com>
2023-04-24 13:31:29 -0400

    Update MIP4 title and corrected end dates for MIP3 and MIP4

---
+++ server/migrations/2023-04-24-165634_update-mip3-mip4/up.sql
@@ -0,0 +1,3 @@
+-- Your SQL goes here
+UPDATE mina_proposals SET end_time = 1683935820000 WHERE key = 'MIP3';
+UPDATE mina_proposals SET title = 'Easier zkApp programmability on mainnet', end_time = 1683935820000 WHERE key = 'MIP4';

b3e2f28d8b48e2e9a683869a851dc2931745b562
jstavh <jstavh@gmail.com>
2023-09-12 16:56:31 -0700

    various changes to titles, nav bar, footer

--- web/common/config.ts
+++ web/common/config.ts
@@ -1,11 +1,7 @@
 export const siteConfig = {
-  title: 'On-Chain Voting',
+  title: 'mina.vote',
   url: 'https://mina.vote/',
   nav: [
-    {
-      title: 'Documentation',
-      href: 'https://github.com/Granola-Team/mina-on-chain-voting/blob/main/README.md',
-    },
     {
       title: 'FAQ',
       href: 'https://forums.minaprotocol.com/t/on-chain-voting-frequently-asked-questions-faq/5959',

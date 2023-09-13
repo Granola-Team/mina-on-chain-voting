437c4c74bb6b3e5ba9ba512081c9a0fba35a24d5
jstavh <jstavh@gmail.com>
2023-09-13 13:11:57 -0700

    results bar

--- web/components/core/progress.tsx
+++ web/components/core/progress.tsx
@@ -12,11 +12,11 @@ const Progress = React.forwardRef<
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn('relative h-2 w-full overflow-hidden rounded-full bg-primary/20', className)}
+    className={cn('relative h-2 w-full overflow-hidden rounded-full bg-logoOrange/20', className)}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className="h-full w-full flex-1 bg-logoOrange transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>

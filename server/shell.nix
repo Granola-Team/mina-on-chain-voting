8250aa14f5a94537b938f49f3fa11c5676f883ce
Robin Bate Boerop <me@robinbb.com>
2023-09-19 23:37:02 -0700

    Improve shell.nix

--- server/shell.nix
+++ server/shell.nix
@@ -2,13 +2,6 @@
 
 let
 
-  # To use this shell.nix on NixOS your user needs to be configured as such:
-  # users.extraUsers.adisbladis = {
-  #   subUidRanges = [{ startUid = 100000; count = 65536; }];
-  #   subGidRanges = [{ startGid = 100000; count = 65536; }];
-  # };
-
-  # Provides a script that copies required files to ~/
   podmanSetupScript = let
     registriesConf = pkgs.writeText "registries.conf" ''
       [registries.search]
@@ -18,7 +11,6 @@ let
     '';
   in pkgs.writeScript "podman-setup" ''
     #!${pkgs.runtimeShell}
-    # Dont overwrite customised configuration
     if ! test -f ~/.config/containers/policy.json; then
       install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
     fi
@@ -27,17 +19,16 @@ let
     fi
   '';
 
-  # Provides a fake "docker" binary mapping to podman
-  dockerCompat = pkgs.runCommandNoCC "docker-podman-compat" {} ''
-    mkdir -p $out/bin
-    ln -s ${pkgs.podman}/bin/podman $out/bin/docker
-  '';
-
 in pkgs.mkShell {
 
   buildInputs = [
-    dockerCompat
-    pkgs.podman  # Docker compat
+    pkgs.cargo-make
+    pkgs.clippy
+    pkgs.openssl
+    pkgs.pkg-config
+    pkgs.podman
+    pkgs.postgresql
+    pkgs.skopeo
 
     # Not on Darwin:
     #
@@ -45,13 +36,6 @@ in pkgs.mkShell {
     # pkgs.conmon  # Container runtime monitor
     # pkgs.slirp4netns  # User-mode networking for unprivileged namespaces
     # pkgs.fuse-overlayfs  # CoW for images, much faster than default vfs
-
-    pkgs.skopeo  # Interact with container registry
-    pkgs.clippy
-    pkgs.cargo-make
-    pkgs.openssl
-    pkgs.pkg-config
-    pkgs.postgresql
   ];
 
   shellHook = ''

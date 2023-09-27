1f874c548be3a9f10ffce76e4cd0cb6a444c692c
jstavh <jstavh@gmail.com>
2023-09-27 16:07:35 -0700

    adding method calculate_total_stake for calculating percent of stake voted to date

--- server/src/models/ledger.rs
+++ server/src/models/ledger.rs
@@ -139,6 +139,93 @@ impl Ledger {
             }
         }
     }
+
+    #[allow(dead_code)] // will remove this method is called from the frontend
+    pub(crate) fn calculate_total_stake(
+        &self,
+        version: &ProposalVersion,
+        map: &Wrapper<HashMap<String, MinaVote>>,
+    ) -> Result<Decimal> {
+        let total_stake =
+            match version {
+                ProposalVersion::V1 => {
+                    self.0
+                        .iter()
+                        .map(|account| {
+                            if account.delegate != account.pk {
+                                Decimal::new(0, LEDGER_BALANCE_SCALE)
+                            } else {
+                                let delegators = self
+                                    .0
+                                    .iter()
+                                    .filter(|d| d.delegate == account.pk && d.pk != account.pk)
+                                    .collect::<Vec<&LedgerAccount>>();
+
+                                if delegators.is_empty() {
+                                    account
+                                        .balance
+                                        .parse::<Decimal>()
+                                        .unwrap_or_else(|_| Decimal::new(0, LEDGER_BALANCE_SCALE))
+                                } else {
+                                    let stake_weight = delegators.iter().fold(
+                                        Decimal::new(0, LEDGER_BALANCE_SCALE),
+                                        |acc, x| {
+                                            x.balance.parse::<Decimal>().unwrap_or_else(|_| {
+                                                Decimal::new(0, LEDGER_BALANCE_SCALE)
+                                            }) + acc
+                                        },
+                                    );
+
+                                    stake_weight
+                                        + account.balance.parse::<Decimal>().unwrap_or_else(|_| {
+                                            Decimal::new(0, LEDGER_BALANCE_SCALE)
+                                        })
+                                }
+                            }
+                        })
+                        .sum()
+                }
+                ProposalVersion::V2 => {
+                    self.0
+                        .iter()
+                        .map(|account| {
+                            if account.delegate != account.pk || map.0.contains_key(&account.pk) {
+                                Decimal::new(0, LEDGER_BALANCE_SCALE)
+                            } else {
+                                let delegators = self
+                                    .0
+                                    .iter()
+                                    .filter(|d| d.delegate == account.pk && d.pk != account.pk)
+                                    .collect::<Vec<&LedgerAccount>>();
+
+                                if delegators.is_empty() {
+                                    account
+                                        .balance
+                                        .parse::<Decimal>()
+                                        .unwrap_or_else(|_| Decimal::new(0, LEDGER_BALANCE_SCALE))
+                                } else {
+                                    let stake_weight = delegators.iter().fold(
+                                        Decimal::new(0, LEDGER_BALANCE_SCALE),
+                                        |acc, x| {
+                                            x.balance.parse::<Decimal>().unwrap_or_else(|_| {
+                                                Decimal::new(0, LEDGER_BALANCE_SCALE)
+                                            }) + acc
+                                        },
+                                    );
+
+                                    stake_weight
+                                        + account.balance.parse::<Decimal>().unwrap_or_else(|_| {
+                                            Decimal::new(0, LEDGER_BALANCE_SCALE)
+                                        })
+                                }
+                            }
+                        })
+                        .sum()
+                }
+            };
+
+        Ok(total_stake)
+    }
 }
 
 #[cfg(test)]

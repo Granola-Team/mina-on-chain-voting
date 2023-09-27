1ceb388ae61035f64f2a9c99c204796d47d9512d
jstavh <jstavh@gmail.com>
2023-09-26 17:56:43 -0700

    removing this code because it appaers to be duplication

--- server/src/main.rs
+++ server/src/main.rs
@@ -25,21 +25,6 @@ extern crate tracing;
 
 pub(crate) const MINA_GOVERNANCE_SERVER: &str = "mina_governance_server";
 
-fn fetch_url_and_print_response() -> Result<(), reqwest::Error> {
-    let url = "https://raw.githubusercontent.com/Granola-Team/mina-ledger/main/{network}/{hash}.json";
-
-    let response = reqwest::blocking::get(url)?;
-
-    if response.status().is_success() {
-        let body = response.text()?;
-        println!("Response:\n{body}");
-    } else {
-        println!("Error: Status Code {:?}", response.status());
-    }
-
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
@@ -53,14 +38,6 @@ async fn main() -> Result<()> {
         target: MINA_GOVERNANCE_SERVER,
         "Initializing database connection pools..."
     );
-
-    // Spawn the URL fetching function concurrently
-    let fetch_task = tokio::spawn(async move {
-        if let Err(err) = fetch_url_and_print_response() {
-            // Handle any errors here
-            eprintln!("Error: {err:?}");
-        }
-    });
 
     let conn_manager = DBConnectionManager::get_connections(&config);
 
@@ -77,9 +54,6 @@ async fn main() -> Result<()> {
     );
 
     serve(router.clone(), config.port).await;
-
-    // Wait for the fetch task to complete before exiting
-    let _ = fetch_task.await;
 
     Ok(())
 }

5c47c596f0e03670411af7ccd025e4f1b044f3ea
Jen Reiss <jenc.reiss24@gmail.com>
2023-10-04 15:13:50 -0500

    add running database restoration tool

--- tools/buildPackages.sh
+++ tools/buildPackages.sh
@@ -4,28 +4,32 @@ buildPhase() {
   echo "building archive node tools..."
   buildArchiveDatabase
   buildArchiveDumpDownloader
+  cleanup()
 }
 
 buildArchiveDatabase() {
     ghc --make ArchiveDatabase -o archive-database
-    rm ./**/*.o
-    rm ./**/*.hi
-    rm *.hi
-    rm *.o
 }
 
 buildArchiveDumpDownloader() {
     ghc --make DownloadArchiveDump -o download-archive-dump
-    rm ./**/*.o
-    rm ./**/*.hi
-    rm *.hi
-    rm *.o
+}
+
+buildDatabaseRestoration() {
+  ghc --make ArchiveDatabaseRestoration -o restore-archive-db
 }
 
 installPhase() {
   mkdir -p $out/bin
   cp archive-database $out/bin
   cp download-archive-dump $out/bin
+}
+
+cleanup() {
+  rm ./**/*.o
+  rm ./**/*.hi
+  rm *.hi
+  rm *.o
 }
 
 genericBuild

9f115f836bf0bad1222fa258fdf7bf50783ab18c
Jen Reiss <jenc.reiss24@gmail.com>
2023-10-10 13:01:57 -0500

    review changes

--- tools/default.nix
+++ tools/default.nix
@@ -6,8 +6,7 @@ pkgs.stdenv.mkDerivation rec {
     bash
     haskell-language-server
     rnix-lsp nixpkgs-fmt
-    geos gdal
-    (postgresql.withPackages (p: [ p.postgis ]))
+    (postgresql.withPackages (p: [ ]))
     (haskellPackages.ghcWithPackages (self: with haskellPackages; [
       effectful curl xml tar zlib megaparsec bytestring directory tmp-postgres json process hlint optparse-applicative
     ]))

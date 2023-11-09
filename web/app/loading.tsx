ceb1a37debc3162541e28599f5997454a1757c63
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2023-11-08 16:29:16 -0800

    forgot to remove extra unused code

--- web/app/loading.tsx
+++ web/app/loading.tsx
@@ -1,15 +1,5 @@
 import './styles.css'
 
-// export default function LoadingDots() {
-//   return (
-//     <div className="flex justify-center items-center min-h-screen">
-//         <div className="space-x-2">
-//           <span className='load-dots'></span>
-//        </div>
-//     </div>
-//   );
-// }
-
 export default function LoadingDots() {
     return (
       <div className="flex justify-center items-center min-h-screen">

f1f283a30b9c7e3c48e26889c575a9d61b643f80
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2023-11-08 16:26:13 -0800

    implemented basic "LoadingDots" loading page

---
+++ web/app/loading.tsx
@@ -0,0 +1,21 @@
+import './styles.css'
+
+// export default function LoadingDots() {
+//   return (
+//     <div className="flex justify-center items-center min-h-screen">
+//         <div className="space-x-2">
+//           <span className='load-dots'></span>
+//        </div>
+//     </div>
+//   );
+// }
+
+export default function LoadingDots() {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="space-x-2">
+          <span className='load-dots w-12 h-12 block mx-auto relative text-yellow-500 box-border animate-rotation'></span>
+        </div>
+      </div>
+    );
+  }

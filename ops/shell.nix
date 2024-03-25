13b1ddbaba05eaaaec1aba747c40b036361fae3b
Robin Bate Boerop <me@robinbb.com>
2024-03-24 17:12:49 -0700

    Support only Docker (#423)

--- ops/shell.nix
+++ ops/shell.nix
@@ -2,42 +2,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/311a4be96d940a0c673e88bd5bc83ea4f005cc02.tar.gz") {}
 }:
 
-let
-
-  podmanSetupScript = let
-    registriesConf = pkgs.writeText "registries.conf" ''
-      [registries.search]
-      registries = ['docker.io']
-      [registries.block]
-      registries = []
-    '';
-    storageConf = pkgs.writeText "storage.conf" ''
-      [storage]
-      driver = "overlay"
-      graphRoot = "$HOME/containers/graphRoot"
-      runRoot = "$HOME/containers/runRoot"
-    '';
-  in pkgs.writeScript "podman-setup" ''
-    #!${pkgs.runtimeShell}
-    if ! test -f ~/.config/containers/policy.json; then
-      install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
-    fi
-    if ! test -f ~/.config/containers/registries.conf; then
-      install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
-    fi
-    if ! test -f ~/.config/containers/storage.conf; then
-      install -Dm555 ${storageConf} ~/.config/containers/storage.conf
-    fi
-  '';
-
-  # Provides fake "docker" and "docker-compose" binaries.
-  dockerCompat = pkgs.runCommandNoCC "docker-podman-compat" {} ''
-    mkdir -p $out/bin
-    ln -s ${pkgs.podman}/bin/podman $out/bin/docker
-    ln -s ${pkgs.podman-compose}/bin/podman-compose $out/bin/docker-compose
-  '';
-
-in pkgs.mkShell {
+pkgs.mkShell {
 
   # See https://github.com/rust-lang/rust-bindgen#environment-variables
 
@@ -63,6 +28,7 @@ in pkgs.mkShell {
 #    ];
 
   buildInputs = [
+    pkgs.cacert
     pkgs.clang
     pkgs.just        # For running the build tool.
     pkgs.libiconv    # Required for compiling Rust tools.
@@ -73,19 +39,13 @@ in pkgs.mkShell {
     pkgs.postgresql  # Required for compiling against libpq, and for pg_isready.
     pkgs.cargo
     pkgs.clippy
-    pkgs.skopeo
-  ] ++ pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
-    pkgs.podman          # Required for testing with containers.
-    pkgs.podman-compose  # Required for testing with containers.
-    dockerCompat
   ] ++ pkgs.lib.optionals (pkgs.stdenv.isDarwin) [
     pkgs.darwin.apple_sdk.frameworks.Security
     pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
   ];
 
   shellHook = ''
-    # Install required configuration
-    ${podmanSetupScript}
-    export TMPDIR=/var/tmp
+    # To avoid running out of space
+    # export TMPDIR=/var/tmp
   '';
 }
